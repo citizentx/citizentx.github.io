91c1e8332e376ad4dffd06cfcf11cb39fd5fcf31
Matthew Feng <mattfeng@mit.edu>
2022-10-19 08:57:52 -0400

    add path

--- public/src/templates/posts.js
+++ public/src/templates/posts.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import Tag from "../components/tag"
@@ -6,57 +6,21 @@ import Box from "../components/box"
 import { Link } from "gatsby"
 
 import "../styles/main.scss"
-
 import * as styles from "../styles/notes.module.scss"
+import "reactflow/dist/style.css"
 
 require("katex/dist/katex.min.css")
 
-const FootnoteContext = createContext([])
-
-const Footnote = ({ id }) => {
-  const footnotes = useContext(FootnoteContext)
-
-  const findIndex = id => {
-    for (const [index, footnote] of footnotes.entries()) {
-      if (id === footnote.id) {
-        return index + 1
-      }
-    }
-    return -1
-  }
-
-  const index = findIndex(id)
-
-  if (index > 0) {
-    return <sup>{findIndex(id)}</sup>
-  }
-
-  return <></>
-}
-
-const shortcodes = { T: Tag, Link, Box, Footnote }
+const shortcodes = { T: Tag, Link, Box }
 
 export default function PageTemplate({ data: { mdx }, children }) {
   console.log(mdx)
 
   return (
     <>
-      <FootnoteContext.Provider value={mdx.frontmatter.footnotes}>
-        <div className={styles.mainContainer}>
-          <MDXProvider components={shortcodes}>{children}</MDXProvider>
-
-          {mdx.frontmatter.footnotes && (
-            <div className={styles.footnotes}>
-              {mdx.frontmatter.footnotes?.map((footnote, idx) => (
-                <p>
-                  <sup>{idx + 1}</sup>
-                  <span dangerouslySetInnerHTML={{ __html: footnote.html }} />
-                </p>
-              ))}
-            </div>
-          )}
-        </div>
-      </FootnoteContext.Provider>
+      <div className={styles.mainContainer}>
+        <MDXProvider components={shortcodes}>{children}</MDXProvider>
+      </div>
     </>
   )
 }
